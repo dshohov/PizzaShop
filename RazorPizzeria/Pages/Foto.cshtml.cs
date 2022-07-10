01dc20d386dbb58a2d74a6eca861b0d23d663af3
Dmytro Shokhov <dshohov@gmail.com>
2022-07-10 15:21:08 +0300

    first commit

---
+++ RazorPizzeria/Pages/Foto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeria.Pages
+{
+    public class FotoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
